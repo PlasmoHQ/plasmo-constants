cb6205e0fba7aba022d95b1d1af066b730be554d
Louis <6723574+louisgv@users.noreply.github.com>
2023-02-09 15:18:18 -0400

    update typing for cs

--- manifest/content-script.ts
+++ manifest/content-script.ts
@@ -1,24 +1,32 @@
-export type ManifestContentScript = {
+export type ManifestContentScript = (
+  | {
+      world: "MAIN"
+      persist_across_sessions?: boolean
+    }
+  | {
+      world?: "ISOLATED"
+
+      match_about_blank?: boolean
+      match_origin_as_fallback?: boolean
+
+      /**
+       * https://developer.chrome.com/docs/extensions/mv3/content_scripts/#matchAndGlob
+       */
+      exclude_globs?: string[]
+      include_globs?: string[]
+    }
+) & {
   matches: string[]
   js?: string[]
 
   css?: string[]
 
-  match_about_blank?: boolean
-  match_origin_as_fallback?: boolean
   all_frames?: boolean
-
-  world?: "ISOLATED" | "MAIN"
 
   /**
    * https://developer.chrome.com/docs/extensions/mv3/content_scripts/#run_time
    */
   run_at?: "document_start" | "document_end" | "document_idle"
 
-  /**
-   * https://developer.chrome.com/docs/extensions/mv3/content_scripts/#matchAndGlob
-   */
   exclude_matches?: string[]
-  exclude_globs?: string[]
-  include_globs?: string[]
 }
