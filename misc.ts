3f81df297a96e478df65a1cf8f1bc6dfa3dc5233
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-04-16 22:17:24 -0400

    fix type-only import

--- misc.ts
+++ misc.ts
@@ -1,8 +1,8 @@
-import type ParcelWatcher from "@parcel/watcher"
+import type { BackendType } from "@parcel/watcher"
 
 import { isLinux, isWindows } from "@plasmo/utils/platform"
 
-export const PARCEL_WATCHER_BACKEND: ParcelWatcher.BackendType = isWindows
+export const PARCEL_WATCHER_BACKEND: BackendType = isWindows
   ? "windows"
   : isLinux
   ? "inotify"
